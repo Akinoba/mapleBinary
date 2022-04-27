a7656c16c3e8e56d51b335617ea45bb07a24f728
KateBin <programming.kaitlyn.williams@gmail.com>
2022-04-27 19:04:51 -0400

    File reading & Logical Error Solving 📝

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import re
 
 #                   < Program settings / directory >
 # This const is not nessecary, however it will make it easier to change the rendered image
-FILE = r"cali_cube.txt"
 
-magnification = 20
+FILE = r"bingus.txt"
+line_data = open(FILE, "r").readlines()
+
+magnification = 4
 is_borderless = False
 window_title = FILE
 
-#                   < Creates a 2D list in "lines" >
+
+#                   < Parsing file and relocatating data >
+# Inorder to store bits in the 2D list "lines", we need to extract the bits from the requested line in "FILE"
+# To get all the bits in a line we grab all the requested characters with a regular expressions pattern
+# Once the all the characters have been grabbed we can set them to a "line" in "lines", this provides ease of acsess
 # By creating a 2D list we can store each line as an index ie: line[1] --> y pos 2
 
-line = []
-lines = [line for line_count in enumerate(open(FILE, "r").readlines())]
-
-
-#for line_count in enumerate(open(FILE, "r").readlines()):
-    #lines.append(line)
+pat = re.compile(r"0|1")
+lines = [pat.findall(line) for line in line_data]
 
 
 #                   < Finding image properties & sizes >
@@ -26,23 +28,12 @@ lines = [line for line_count in enumerate(open(FILE, "r").readlines())]
 # To avoid the indexing of non existant data we need to check if there is any data,
 # We can do this by checking if the list is False (empty), if not empty --> continue
 
-pat = re.compile(r"0|1")
-
-if(bool(open(FILE, "r").readlines()[0]) != False):
-    x_size = len(pat.findall(open(FILE, "r").readlines()[0])) # Grab length of line
-    y_size = len(open(FILE, "r").readlines()) # Grab number of lines
+if(bool(line_data[0]) != False):
+    x_size = len(lines[0]) # Grab length of line
+    y_size = len(lines) # Grab number of lines
 
 else:
     raise RuntimeError(":: FILE MUST CONTAIN BINARY DATA ::")
-
-
-#                   < Parsing file and relocatating data >
-# Inorder to store bits in the 2D list "lines", we need to extract the bits from the requested line in "FILE"
-# To get all the bits in a line we grab all the requested characters with a regular expressions pattern
-# Once the all the characters have been grabbed we can set them to a "line" in "lines", this provides ease of acsess
-
-for y_count in range(y_size):
-    lines[y_count] = pat.findall(open(FILE, "r").readlines()[y_count])
 
 
 #                   < Creating window & setting magnifaction >
@@ -51,13 +42,9 @@ for y_count in range(y_size):
 # x_size & y_size to strings. If magnifaction is required --> multiply value by magnification value --> convert to string
 # Note magnification is pseudo, in reality the size of the pixel is being changed, essiently this enlarges an image.
 
-x_size = x_size * magnification
-y_size = y_size * magnification
-
-
 window = tk.Tk()
 window.title(window_title)
-window.geometry(str(x_size) + "x" + str(y_size))
+window.geometry(str(x_size * magnification) + "x" + str(y_size * magnification))
 window.overrideredirect(is_borderless) # if true --> set window to borderless
 
 
@@ -67,7 +54,7 @@ window.overrideredirect(is_borderless) # if true --> set window to borderless
 # Since the tkinter Canvas has no direct function to render a pixel we can utlize the create_rectangle function instead
 # This provides us with benfits a pixel function can not, for example: magnify a pixel --> we can change the 2nd set of x & y cords
 
-viewport = tk.Canvas(window, width=str(x_size), height=str(y_size), bg="black", highlightthickness=0)
+viewport = tk.Canvas(window, width=str(x_size * magnification), height=str(y_size * magnification), bg="black", highlightthickness=0)
 
 def draw_pixel(x, y, bit):
     if(str(bit) == "1"):
@@ -81,8 +68,8 @@ def draw_pixel(x, y, bit):
 # To render each pixel to the canvas (viewport) we need to utlize for-loops to itterate through the y_size & x_size
 # For every "line" in the "lines" we need to print each bit as a pixel, to do this we can call the draw_pixel function definded above
 
-for y_count in range(len(lines[y_count])):
-    for x_count in range(len(lines[y_count])):
+for y_count in range(y_size):
+    for x_count in range(x_size):
         draw_pixel(x_count * magnification, y_count * magnification, lines[y_count][x_count])
 
 viewport.pack()
@@ -90,4 +77,4 @@ window.mainloop()
 
 # Kaitlyn Williams 2022
 # Apache 2.0 License
-# ♻️ loop optimization ♻️
+# 📝 File reading & Logical Error Solving 📝
